8c1884a900847d162ffd9c21ffb9f6620d057150
Kyle Reynoso <kmreynoso@up.edu.ph>
2022-05-14 01:07:22 +0800

    Merge HoR and bills data into tentative dataset

--- congress-members-rip/cong_combine.py
+++ congress-members-rip/cong_combine.py
@@ -8,11 +8,11 @@ df1.loc[df1['rep_region']=='Taguig', 'rep_subdistrict'] = 'Lone District'
 new_df = pd.merge(df1, df2,  how='left', left_on=['rep_subdistrict','rep_region'], right_on = ['district', 'subdistrict'])
 # from wikipedia
 new_df.loc[new_df['rep_name']=='Mercado, Roger G.', 'party'] = 'LAKAS'
-new_df.loc[new_df['rep_name']=='Mercado, Roger G.', 'subregion'] = 'Southern Leyte'
-new_df.loc[new_df['rep_name']=='Mercado, Roger G.', 'subdistrict'] = 'Lone District'
+new_df.loc[new_df['rep_name']=='Mercado, Roger G.', 'subdistrict'] = 'Southern Leyte'
+new_df.loc[new_df['rep_name']=='Mercado, Roger G.', 'district'] = 'Lone District'
 new_df.loc[new_df['rep_name']=='Cua, Junie E.', 'party'] = 'PDP–Laban'
-new_df.loc[new_df['rep_name']=='Cua, Junie E.', 'subregion'] = 'Quirino'
-new_df.loc[new_df['rep_name']=='Cua, Junie E.', 'subdistrict'] = 'Lone District'
+new_df.loc[new_df['rep_name']=='Cua, Junie E.', 'subdistrict'] = 'Quirino'
+new_df.loc[new_df['rep_name']=='Cua, Junie E.', 'district'] = 'Lone District'
 toresolve = new_df[new_df['subdistrict'].isna() & new_df['partylist'].isna()]
 assert len(toresolve)==0
 new_df.to_csv('18th_congress_hor.csv', index=False)
